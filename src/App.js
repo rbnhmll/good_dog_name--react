76828f71fe33c9c37617dc9d003ecc0fc1a28058
Robin Hamill <hamillrobin@gmail.com>
2017-08-04 09:55:13 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Good dog/cat name!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
